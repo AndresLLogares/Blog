7c8dd3ca7ec1b749c1774b0dc6b3ce6ad2485c73
AndresLLogares <andresl940@hotmail.com>
2021-05-20 16:18:08 -0300

    Jueves 20/05 16:18

--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -24,8 +24,13 @@ const NavigationBar = () => {
             </div>
             <ul className={click ? styles.nav_options_active : styles.nav_options}>
                 <li className={styles.SortNav} onClick={closeMobileMenu} >
-                    <a style={{ textDecoration: 'none' }} href='https://www.linkedin.com/in/andres-luis-logares-522595172/detail/overlay-view/urn:li:fsd_profileTreasuryMedia:(ACoAACkFWaoBglZUp19GGG5Q3LzLSMviGHXP1nw,1635457761733)/' >
+                    <a style={{ textDecoration: 'none' }} href='https://drive.google.com/file/d/1ZrRdklAvt_G1hbYRo4tlCBAQUKdmIIvs/view?usp=sharing' >
                         <div className={styles.Links} ><TextDocument className={styles.IconsNav} />CV</div>
+                    </a>
+                </li>
+                <li className={styles.SortNav} onClick={closeMobileMenu} >
+                    <a style={{ textDecoration: 'none' }} href='https://drive.google.com/file/d/1eTHW-prsSwCMw7aAnNnCzCeQbTTtZNBO/view?usp=sharing' >
+                        <div className={styles.Links} ><TextDocument className={styles.IconsNav} />English CV</div>
                     </a>
                 </li>
                 <li className={styles.SortNav} onClick={closeMobileMenu} >
