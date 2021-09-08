424a9f1ae84d2f25da99ac9a4ba654cd8165df3c
AndresLLogares <Andresl940@hotmail.com>
2021-09-07 23:07:16 -0300

    martes 7/9 23:07

--- src/App.js
+++ src/App.js
@@ -1,38 +1,37 @@
-import React from 'react';
-import { Route, BrowserRouter as Router, Switch, Redirect, HashRouter } from 'react-router-dom';
-import Home from './Components/Home/Home';
-import NavigationBar from './Components/Nav/Nav';
-import PageNotFound from './Components/PageNotFound/PageNotFound';
-import LandPage from './Components/LandPage/LandPage';
-import HomeEnglish from './Components/HomeEnglish/HomeEnglish';
-import Projects from './Components/Projects/Projects';
-import ProjectsE from './Components/ProjectsE/ProjectsE';
-import ScrollToTop from './Components/ScrolToTop/ScrolToTop';
-import NavE from './Components/Nav/NavE';
-import './Scss/App/App.css';
+import React from "react";
+import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
+import Home from "./Components/Home/Home";
+import NavigationBar from "./Components/Nav/Nav";
+import LandPage from "./Components/LandPage/LandPage";
+import HomeEnglish from "./Components/HomeEnglish/HomeEnglish";
+import Projects from "./Components/Projects/Projects";
+import ProjectsE from "./Components/ProjectsE/ProjectsE";
+import ScrollToTop from "./Components/ScrolToTop/ScrolToTop";
+import NavE from "./Components/Nav/NavE";
+import "./Scss/App/App.css";
 
 function App() {
   return (
     <Router>
-      <Route path='/Home'>
+      <Route path="/Home">
         <NavigationBar />
       </Route>
-      <Route path='/HomeE'>
+      <Route path="/HomeE">
         <NavE />
       </Route>
-      <Route path='/Projects'>
+      <Route path="/Projects">
         <NavigationBar />
       </Route>
-      <Route path='/ProjectsE'>
+      <Route path="/ProjectsE">
         <NavE />
       </Route>
       <ScrollToTop />
       <Switch>
-        <Route exact path='/' component={LandPage} />
-        <Route exact path='/Home' component={Home} />
-        <Route exact path='/HomeE' component={HomeEnglish} />
-        <Route exact path='/Projects' component={Projects} />
-        <Route exact path='/ProjectsE' component={ProjectsE} />
+        <Route exact path="/" component={LandPage} />
+        <Route exact path="/Home" component={Home} />
+        <Route exact path="/HomeE" component={HomeEnglish} />
+        <Route exact path="/Projects" component={Projects} />
+        <Route exact path="/ProjectsE" component={ProjectsE} />
       </Switch>
     </Router>
   );
